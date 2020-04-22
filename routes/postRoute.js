b4c53f72aeab30becd7bbe7d6e965ce118b9f684
Timo <timo.schessl@gmx.de>
2020-04-22 18:30:41 +0200

    changed postRoute for new PostModel

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -2,29 +2,31 @@ const express = require("express")
 const router = express.Router()
 const PostModel = require("../models/PostModel")
 
-router.post("/", (request, response)=>
-{
-    console.log("The post request is: "+request.body)
+router.post("/", (request, response) => {
+    console.log("The post request is: " + request.body)
 
     const postaction = new PostModel({
-        title: request.body.title,
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        age: request.body.age,
+        birthday: request.body.birthday,
+        job: request.body.job,
         description: request.body.description
     })
 
-    
-    postaction.save().then(result =>
-        {
-            response.json(result)
-            console.log(result)
-            console.log("Post request successful.")
-        })
+
+    postaction.save().then(result => {
+        response.json(result)
+        console.log(result)
+        console.log("Post request successful.")
+    })
         .catch(error => {
-            
+
             response.json(error)
             console.log(error)
         })
 
-    
+
 })
 
 
