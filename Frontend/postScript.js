ea22eb87f424a29c69f42dafd63f852b748e26dc
Timo <timo.schessl@gmx.de>
2020-04-22 18:58:22 +0200

    Working on postScript

---
+++ Frontend/postScript.js
@@ -0,0 +1,27 @@
+function postData() {
+
+    var firstNameV = document.getElementById("firstName").value;
+    var lastNameV = document.getElementById("lastName").value;
+    var ageV = document.getElementById("age").value;
+    var birthdayV = document.getElementById("birthday").value;
+    var jobV = document.getElementById("job").value;
+    var descriptionV = document.getElementById("description").value;
+
+    fetch("http://localhost:2000/post", {
+        method: 'POST', body:
+        {
+             firstName: firstNameV,
+             lastName: lastNameV,
+             age: ageV,
+             birthday: birthdayV,
+             job: jobV,
+             description: descriptionV
+        }
+    })
+        .then(result => {
+            console.log(result)
+            result.text().then(text => { document.getElementById("showData").value = text })
+        })
+
+
+}
