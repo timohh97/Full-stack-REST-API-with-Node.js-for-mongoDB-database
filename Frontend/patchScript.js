63a2e6bd77a6c9d42ad28f980e323c8b6b055673
Timo <timo.schessl@gmx.de>
2020-04-22 19:44:41 +0200

    Patching works

---
+++ Frontend/patchScript.js
@@ -0,0 +1,32 @@
+function patchData() {
+
+    var Id = document.getElementById("Id").value;
+    var firstNameV = document.getElementById("firstName").value;
+    var lastNameV = document.getElementById("lastName").value;
+    var ageV = document.getElementById("age").value;
+    var birthdayV = document.getElementById("birthday").value;
+    var jobV = document.getElementById("job").value;
+    var descriptionV = document.getElementById("description").value;
+
+
+    fetch("http://localhost:2000/patch/"+Id, {
+        method: 'PATCH', 
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            
+            firstName: firstNameV,
+            lastName: lastNameV,
+            age:  ageV,
+            birthday: birthdayV,
+            job: jobV,
+            description: descriptionV
+        })
+        
+    })
+        .then(result => {
+            console.log(result)
+            result.text().then(text => { document.getElementById("showData").value = text })
+        })
+
+
+}
